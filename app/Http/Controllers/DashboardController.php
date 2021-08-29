19833621494297aed8a1129c8c13c9626621c611
Luis Antonio Parrado <luisprmat@gmail.com>
2021-08-29 15:56:33 -0500

    Create form for messages

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('dashboard');
+        $users = User::where('id', '<>', auth()->id())->orderBy('name')->get();
+
+        return view('dashboard', compact('users'));
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,9 +6,53 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+        <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-4">
+                <form action="{{ route('messages.store') }}" method="POST">
+                    @csrf
+                    <div class="mb-4">
+                        <x-jet-label>
+                            Asunto
+                        </x-jet-label>
+
+                        <x-jet-input type="text"
+                            class="w-full"
+                            placeholder="Ingrese el asunto"
+                            name="subject"
+                        />
+                    </div>
+
+                    <div class="mb-4">
+                        <x-jet-label>
+                            Mensaje
+                        </x-jet-label>
+
+                        <textarea class="form-control w-full"
+                            name="body"
+                            rows="4"
+                            placeholder="Escriba su mensaje"
+                        ></textarea>
+                    </div>
+
+                    <div class="mb-4">
+                        <x-jet-label>
+                            Destinatario
+                        </x-jet-label>
+
+                        <select name="to_user_id"
+                            class="form-control w-full"
+                        >
+                            <option value="" selected disabled>Seleccione un usuario</option>
+                            @foreach ($users as $user)
+                                <option value="{{ $user->id }}">{{ $user->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+
+                    <x-jet-button>
+                        Enviar
+                    </x-jet-button>
+                </form>
             </div>
         </div>
     </div>
